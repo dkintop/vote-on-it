eef07a5b855c6fa0f23f62c7f083f4e817029021
Doug Kintop <dkintop@hotmail.com>
2019-12-02 15:05:35 -0500

    created form for new subject creation, need to implement nested attributes to simultaneously create options

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -2,8 +2,19 @@ class SubjectsController < ApplicationController
 
     def new 
         #renders /subjects/new (form page)
+        @subject = Subject.new
     end
 
-    
+    def create
+        binding.pry
+    end
+
+private
+
+def subject_params
+
+    params.require(:subject).permit(:title, :category, :description)
+
+end
 
 end
