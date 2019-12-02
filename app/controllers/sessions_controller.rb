54093c6608ea7655213292706669865e5bc0b88c
Doug Kintop <dkintop@hotmail.com>
2019-12-02 12:51:41 -0500

    finished login and logout features, insurance commit before attempting to implement bootstrat theme

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,14 @@ class SessionsController < ApplicationController
     end
 
     def login
-        @user = User.find_by(username: params[] )
+        @user= User.find_by(email: session_params[:email])
+        if @user && @user.authenticate(session_params[:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            flash[:error] = "Sorry, username or password was incorrect. Please try again."
+            redirect_to login_path
+        end
     end
 
     def destroy 
